4fe811e827caab962e4a09ffc13b934ed325dca0
Mohd Sohail Ahmed <63498879+sohail131@users.noreply.github.com>
2021-02-04 00:30:34 +0530

    First Update
    First Update 4-Feb-2021

--- code/hangman.py
+++ code/hangman.py
@@ -19,6 +19,8 @@ class HangmanWindow(BoxLayout):
         self.guess_left, self.hints_left = 5, 5
         self.score = 0
         self.button_pressed, self.underscores_list, self.correct_words = [], [], []
+        self.letters = list(ascii_lowercase)
+        self.running = True
 
     # Show hints
     def show_hint(self):
@@ -32,7 +34,8 @@ class HangmanWindow(BoxLayout):
                     self.correct_words.append(self.word[i])
                     break
         else:
-            self.show_popup(title="No hints left", message="You don't have hints left")
+            self.running = False
+            self.show_popup(title="No hints left", message="You don't have any hints left")
 
     # Change welcome screen to name screen
     def change_screen(self):
@@ -47,13 +50,18 @@ class HangmanWindow(BoxLayout):
                               background_color=(.06, .47, .47, 1))
         layout.add_widget(popup_label)
         layout.add_widget(popup_button)
-        popup = Popup(title=title, content=layout, size_hint=(None, None), size=(300, 300))
+        popup = Popup(title=title, content=layout, size_hint=(None, None), size=(300, 300), auto_dismiss=False)
         popup.open()
+        popup_button.bind(on_press=self.run_fun)
         popup_button.bind(on_press=popup.dismiss)
+
+    def run_fun(self, *args):
+        self.running = True
 
     # Check valid user name
     def valid_user_name(self):
         self.user_name = str(self.ids.user_name.text)
+
         if len(self.user_name) > 0:
             self.ids.screen_manager.current = "game_screen"
             self.ids.label_user_name.text = "Welcome " + self.user_name
@@ -66,18 +74,17 @@ class HangmanWindow(BoxLayout):
 
     # For handling keyboard events
     def continuous(self):
-        letters = list(ascii_lowercase)
-
         def on_press(key):
             pass
 
         def on_release(key):
-            if "Key" not in str(key):
-                if str(key)[1] in letters:
-                    self.button_pressed.append(str(key)[1].upper())
-                    self.check_word_pressed()
-                if key == Key.esc:
-                    exit()
+            if self.running:
+                if "Key" not in str(key):
+                    if str(key)[1] in self.letters:
+                        self.button_pressed.append(str(key)[1].upper())
+                        self.check_word_pressed()
+                    if key == Key.esc:
+                        exit()
 
         # Collect events until released
         with Listener(on_press=on_press, on_release=on_release) as listener:
@@ -96,43 +103,48 @@ class HangmanWindow(BoxLayout):
 
     # Handling game keyboard events
     def text_button_pressed(self, instance):
-        self.button_pressed.append(instance.text)
-        self.check_word_pressed()
+        if self.running:
+            self.button_pressed.append(instance.text)
+            self.check_word_pressed()
 
     # Checking if the alphabet is present in the word or not
     def check_word_pressed(self):
-        new_word = self.button_pressed[len(self.button_pressed) - 1]
-        if new_word in self.word:
-            for i in range(len(self.word)):
-                if self.word[i] in new_word:
-                    self.underscores_list[i] = new_word
-                    self.correct_words.append(new_word)
-            self.ids.word_length_underscores.text = "    ".join(self.underscores_list)
-            result_str = "".join(self.underscores_list)
-            if result_str == self.word:
-                self.score = self.score + len(self.word) - (5 - self.guess_left)
-                self.ids.display_score.text = f"Score: {str(self.score)}"
-                self.show_popup(title="Hurry! You Won", message=f"The word was {self.word}")
-                self.play_again()
-        else:
-            if self.guess_left == 1:
-                self.score = self.score + len(self.word) - (5 - self.guess_left)
-                self.ids.display_score.text = f"Score: {str(self.score)}"
-                self.show_popup(title="Oops Game Over", message=f"The word was {self.word}")
-                self.play_again()
+        if self.running:
+            new_word = self.button_pressed[len(self.button_pressed) - 1]
+
+            if new_word in self.word:
+                for i in range(len(self.word)):
+                    if self.word[i] in new_word:
+                        self.underscores_list[i] = new_word
+                        self.correct_words.append(new_word)
+
+                self.ids.word_length_underscores.text = "    ".join(self.underscores_list)
+                result_str = "".join(self.underscores_list)
+
+                if result_str == self.word:
+                    self.score = self.score + len(self.word) - (5 - self.guess_left)
+                    self.ids.display_score.text = f"Score: {str(self.score)}"
+                    self.running = False
+                    self.show_popup(title="Hurry! You Won", message=f"The word was {self.word}")
+                    self.play_again()
             else:
-                if new_word not in self.wrong_guessed_words:
-                    self.guess_left -= 1
-                    self.wrong_guessed_words += new_word + " "
-                    self.ids.wrong_guess.text = self.wrong_guessed_words
-                    self.ids.guess_left.text = str(self.guess_left)
+                if self.guess_left == 1:
+                    self.score = self.score + len(self.word) - (5 - self.guess_left)
+                    self.ids.display_score.text = f"Score: {str(self.score)}"
+                    self.running = False
+                    self.show_popup(title="Oops Game Over", message=f"The word was {self.word}")
+                    self.play_again()
+                else:
+                    if new_word not in self.wrong_guessed_words:
+                        self.guess_left -= 1
+                        self.wrong_guessed_words += new_word + " "
+                        self.ids.wrong_guess.text = self.wrong_guessed_words
+                        self.ids.guess_left.text = str(self.guess_left)
 
     # For play again
     def play_again(self):
         self.guess_left = 5
-        self.wrong_guessed_words = ""
-        self.word = ""
-        self.ids.wrong_guess.text = ""
+        self.wrong_guessed_words = self.word = self.ids.wrong_guess.text = ""
         self.button_pressed.clear()
         self.underscores_list.clear()
         self.play_game()
